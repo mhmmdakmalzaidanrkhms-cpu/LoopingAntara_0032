15ca49a7aaf25ee957726c61e61ea8528bd5bff4
mhmmdakmalzaidanrkhms <mhmmdakmalzaidanrkhms@gmail.com>
2026-07-21 10:40:13 +0700

    Add main function and for loop implementation to display iteration values

--- loopingafor_0032.cpp
+++ loopingafor_0032.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+    cout << "PERULANGAN FOR" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Nilai i : " << i << " Teknologi Informasi" << endl;
+
+    }
+    
