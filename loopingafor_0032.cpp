f58535f73490568f7c58b69f1960865307842808
mhmmdakmalzaidanrkhms <mhmmdakmalzaidanrkhms@gmail.com>
2026-07-21 10:39:54 +0700

    Remove main function and for loop implementations from loopingafor_0032.cpp

--- loopingafor_0032.cpp
+++ loopingafor_0032.cpp
@@ -1,20 +1,3 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "PERULANGAN FOR" << endl;
-    for (int i = 0; i < 5; i++) {
-        cout << "Nilai i : " << i << " Teknologi Informasi" << endl;
-
-    }
-    cout << endl;
-          // for untuk turun
-    cout << "PERULANGAN FOR" << endl;
-    for (int i = 5; i > 0; i--) {
-        cout << "Nilai i : " << i << " Teknologi Informasi" << endl;
-
-    }
-    cout << endl;
-
-    return 0;
-}
